78f991a7048ce81fb2c002b546941a9041ca2975
Toke Jepsen <tokejepsen@gmail.com>
2018-01-24 14:41:30 +0100

    Separate environment setup from environment variables.

---
+++ environmen_setup.py
@@ -0,0 +1,95 @@
+import subprocess
+import os
+import shutil
+import sys
+
+from conda_git_deployment import utils
+
+
+def _build_rv_plugin():
+    root = os.path.dirname(__file__)
+
+    subprocess.call(
+        ["python", "setup.py", "build_plugin"],
+        cwd=os.path.join(
+            os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-rv"
+        )
+    )
+
+    # Clear existing plugin
+    path = os.path.join(root, "environment", "RV_SUPPORT_PATH")
+    if os.path.exists(path):
+        shutil.rmtree(path, ignore_errors=True)
+
+    # Copy build plugin
+    sys.path.append(
+        os.path.join(
+            os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-rv", "source"
+        )
+    )
+    import ftrack_connect_rv
+    rvpkg_version = '.'.join(ftrack_connect_rv.__version__.split('.'))
+    plugin_file = 'ftrack-{0}.rvpkg'.format(rvpkg_version)
+    src = os.path.join(
+        os.environ["CONDA_GIT_REPOSITORY"],
+        "ftrack-connect-rv",
+        "build",
+        plugin_file
+    )
+    dst = os.path.join(
+        root, "environment", "RV_SUPPORT_PATH", "Packages", plugin_file
+    )
+
+    if not os.path.exists(os.path.dirname(dst)):
+        os.makedirs(os.path.dirname(dst))
+
+    shutil.move(src, dst)
+
+
+def build_rv_plugin():
+    # Check is ftrack_connect_rv is available for building plugin.
+    if utils.check_module("ftrack_connect_rv"):
+        # Build RV plugin if updating the repositories
+        if ("CONDA_GIT_UPDATE" in os.environ and
+           "repositories" in os.environ["CONDA_GIT_UPDATE"]):
+            build_rv_plugin()
+
+        # Build RV plugin if none exists
+        path = os.path.join(
+            os.path.dirname(__file__), "environment", "RV_SUPPORT_PATH"
+        )
+        if not os.path.exists(path):
+            build_rv_plugin()
+    else:
+        msg = "Could not build ftrack-connect-rv plugin because "
+        msg += "ftrack_connect_rv in not available."
+        print msg
+
+
+def build_ftrack_connect_resources():
+    # Build ftrack-connect resources
+    resources = os.path.join(
+        os.environ["CONDA_GIT_REPOSITORY"],
+        "ftrack-connect",
+        "source",
+        "ftrack_connect",
+        "ui",
+        "resource.py"
+    )
+    if not os.path.exists(resources):
+        subprocess.call(
+            [
+                "python",
+                os.path.join(
+                    os.environ["CONDA_GIT_REPOSITORY"],
+                    "ftrack-connect",
+                    "setup.py"
+                ),
+                "build_resources"
+            ]
+        )
+
+
+if __name__ == "__main__":
+    build_rv_plugin()
+    build_ftrack_connect_resources()
--- environment.py
+++ environment.py
@@ -1,132 +1,68 @@
 import os
-import subprocess
-import shutil
-import sys
 
 from conda_git_deployment import utils
+import environment_setup
 
 
-root = os.path.dirname(__file__)
-env = {}
+def main():
+    root = os.path.dirname(__file__)
+    env = {}
 
-
-def build_rv_plugin():
-
-    subprocess.call(
-        ["python", "setup.py", "build_plugin"],
-        cwd=os.path.join(
-            os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-rv"
-        )
-    )
-
-    # Clear existing plugin
-    path = os.path.join(root, "environment", "RV_SUPPORT_PATH")
-    if os.path.exists(path):
-        shutil.rmtree(path, ignore_errors=True)
-
-    # Copy build plugin
-    sys.path.append(
+    # FTRACK_CONNECT_PLUGIN_PATH
+    env["FTRACK_CONNECT_PLUGIN_PATH"] = [
         os.path.join(
-            os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-rv", "source"
+            os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-maya"
+        ),
+        os.path.join(
+            os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-nuke"
+        ),
+        os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-rv"),
+        os.path.join(
+            os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-nuke-studio"
+        ),
+        os.path.join(root, "environment", "FTRACK_CONNECT_PLUGIN_PATH"),
+        os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect")
+    ]
+
+    # FTRACK_CONNECT_MAYA_PLUGINS_PATH
+    env["FTRACK_CONNECT_MAYA_PLUGINS_PATH"] = [
+        os.path.join(
+            os.environ["CONDA_GIT_REPOSITORY"],
+            "ftrack-connect-maya",
+            "resource"
         )
-    )
-    import ftrack_connect_rv
-    rvpkg_version = '.'.join(ftrack_connect_rv.__version__.split('.'))
-    plugin_file = 'ftrack-{0}.rvpkg'.format(rvpkg_version)
-    src = os.path.join(
-        os.environ["CONDA_GIT_REPOSITORY"],
-        "ftrack-connect-rv",
-        "build",
-        plugin_file
-    )
-    dst = os.path.join(
-        root, "environment", "RV_SUPPORT_PATH", "Packages", plugin_file
-    )
+    ]
 
-    if not os.path.exists(os.path.dirname(dst)):
-        os.makedirs(os.path.dirname(dst))
+    # FTRACK_CONNECT_NUKE_PLUGINS_PATH
+    env["FTRACK_CONNECT_NUKE_PLUGINS_PATH"] = [
+        os.path.join(
+            os.environ["CONDA_GIT_REPOSITORY"],
+            "ftrack-connect-nuke",
+            "resource"
+        )
+    ]
 
-    shutil.move(src, dst)
+    # FTRACK_CONNECT_NUKE_STUDIO_PATH
+    env["FTRACK_CONNECT_NUKE_STUDIO_PATH"] = [
+        os.path.join(
+            os.environ["CONDA_GIT_REPOSITORY"],
+            "ftrack-connect-nuke-studio",
+            "resource"
+        )
+    ]
 
+    # QT_PREFERRED_BINDING
+    env["QT_PREFERRED_BINDING"] = ["PySide2", "PySide"]
 
-# Check is ftrack_connect_rv is available for building plugin.
-if utils.check_module("ftrack_connect_rv"):
-    # Build RV plugin if updating the repositories
-    if ("CONDA_GIT_UPDATE" in os.environ and
-       "repositories" in os.environ["CONDA_GIT_UPDATE"]):
-        build_rv_plugin()
+    # RV_SUPPORT_PATH
+    env["RV_SUPPORT_PATH"] = [
+        os.path.join(root, "environment", "RV_SUPPORT_PATH")
+    ]
 
-    # Build RV plugin if none exists
-    path = os.path.join(root, "environment", "RV_SUPPORT_PATH")
-    if not os.path.exists(path):
-        build_rv_plugin()
-else:
-    msg = "Could not build ftrack-connect-rv plugin because "
-    msg += "ftrack_connect_rv in not available."
-    print msg
+    utils.write_environment(env)
 
 
-# Build ftrack-connect resources
-resources = os.path.join(
-    os.environ["CONDA_GIT_REPOSITORY"],
-    "ftrack-connect",
-    "source",
-    "ftrack_connect",
-    "ui",
-    "resource.py"
-)
-if not os.path.exists(resources):
-    subprocess.call(
-        [
-            "python",
-            os.path.join(
-                os.environ["CONDA_GIT_REPOSITORY"],
-                "ftrack-connect",
-                "setup.py"
-            ),
-            "build_resources"
-        ]
-    )
-
-# FTRACK_CONNECT_PLUGIN_PATH
-env["FTRACK_CONNECT_PLUGIN_PATH"] = [
-    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-maya"),
-    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-nuke"),
-    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-rv"),
-    os.path.join(
-        os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-nuke-studio"
-    ),
-    os.path.join(root, "environment", "FTRACK_CONNECT_PLUGIN_PATH"),
-    os.path.join(os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect")
-]
-
-# FTRACK_CONNECT_MAYA_PLUGINS_PATH
-env["FTRACK_CONNECT_MAYA_PLUGINS_PATH"] = [
-    os.path.join(
-        os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-maya", "resource"
-    )
-]
-
-# FTRACK_CONNECT_NUKE_PLUGINS_PATH
-env["FTRACK_CONNECT_NUKE_PLUGINS_PATH"] = [
-    os.path.join(
-        os.environ["CONDA_GIT_REPOSITORY"], "ftrack-connect-nuke", "resource"
-    )
-]
-
-# FTRACK_CONNECT_NUKE_STUDIO_PATH
-env["FTRACK_CONNECT_NUKE_STUDIO_PATH"] = [
-    os.path.join(
-        os.environ["CONDA_GIT_REPOSITORY"],
-        "ftrack-connect-nuke-studio",
-        "resource"
-    )
-]
-
-# QT_PREFERRED_BINDING
-env["QT_PREFERRED_BINDING"] = ["PySide2", "PySide"]
-
-# RV_SUPPORT_PATH
-env["RV_SUPPORT_PATH"] = [os.path.join(root, "environment", "RV_SUPPORT_PATH")]
-
-utils.write_environment(env)
+if __name__ == "__main__":
+    environment_setup.build_rv_plugin()
+    environment_setup.build_ftrack_connect_resources()
+    main()
